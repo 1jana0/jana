b26646fed330f3c39df4ae6920bebc71595c3048
JANARTHANAN S <113625244+1jana0@users.noreply.github.com>
2022-11-26 12:41:21 +0530

    Add files via upload

---
+++ guess.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+ int i,n,m=56;
+ for(i=0;i<=20;i++)
+ {
+   printf("\n Enter your guess :");
+   scanf("%d", &n);
+   if(n==m)
+   {
+    printf("\n Your Guess is correct");
+    break;
+   }
+   else if(n<m)
+   {
+    printf("\n Your guess is low");
+   }
+   else
+   {
+    printf("\n Your guess is high");
+   }
+ }
+ printf("\n Congragulation You Won");
+}
